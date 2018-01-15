435825b91e98f3a71541d059d1796be619a9c174
Fu <youtianzhan@126.com>
2018-01-15 17:17:51 +0800

    add 2nd step

--- routes/Oauth2.js
+++ routes/Oauth2.js
@@ -5,5 +5,7 @@ const authorize = require('../lib/authorize');
 
 /* GET users listing. */
 router.get('/authorize', middlewares.ensureLogin, authorize.checkAuthorizeParams, authorize.showAppInfo);
+router.post('/authorize', middlewares.ensureLogin,authorize.checkAuthorizeParams, authorize.confirmApp);
+router.post('/access_token', authorize.getAccessToken);
 
 module.exports = router;
